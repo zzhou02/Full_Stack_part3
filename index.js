4fa1210c66cdcf81e7927c640b6258e08f196fc9
Bobby Zhou <bobbyzhouzijun@icloud.com>
2020-04-12 21:35:11 -0700

    3.1-3.6 done

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/info', (req, res) => {
+  const time = new Date()
+  res.send(`<div>Phonebook has info for ${persons.length} people</div><div>${time}</div>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body) {
+    return res.status(400).json({ error: 'content missing' })
+  } else if (!body.name) {
+    return res.status(400).json({ error: 'name missing' })
+  } else if (!body.number) {
+    return res.status(400).json({ error: 'number missing' })
+  } else if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: Math.floor(Math.random() * 10000)
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
