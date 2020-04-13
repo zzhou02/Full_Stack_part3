0e6375691439b75c928a8f34d7679afba21c99fd
Bobby Zhou <bobbyzhouzijun@icloud.com>
2020-04-13 15:58:21 -0700

    3.7-8 done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+// app.use(morgan('tiny'))
+morgan.token('body', (req, res) => {
+  if (req.method === 'POST'){
+    return JSON.stringify(req.body)
+  }
+  
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 let persons = [
   {
     "name": "Arto Hellas",
