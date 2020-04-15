57555e2686c0a61d051bec2e85dfadcb1134bc96
Bobby Zhou <bobbyzhouzijun@icloud.com>
2020-04-15 00:01:05 -0700

    3.12 -14 done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const entry = require('./models/entry')
 
 app.use(express.static('build'))
 app.use(cors())
@@ -13,76 +15,82 @@ morgan.token('body', (req, res) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
+// let persons = [
+//   {
+//     "name": "Arto Hellas",
+//     "number": "040-123456",
+//     "id": 1
+//   },
+//   {
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523",
+//     "id": 2
+//   },
+//   {
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345",
+//     "id": 3
+//   },
+//   {
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122",
+//     "id": 4
+//   }
+// ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  entry.find({}).then(entries => {
+    res.json(entries.map(entry => entry.toJSON()))
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  const id = req.params.id
+  entry.findById(id).then(person => {
+    if (person) {
+      res.json(person.toJSON())
+    } else {
+      res.status(404).end()
+    }
+  })
 })
 
 app.get('/info', (req, res) => {
   const time = new Date()
-  res.send(`<div>Phonebook has info for ${persons.length} people</div><div>${time}</div>`)
+  entry.find({}).then(entries => {
+    res.send(`<div>Phonebook has info for ${entries.length} people</div><div>${time}</div>`)
+  })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
-})
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = req.params.id
+//   persons = persons.filter(person => person.id !== id)
+//   res.status(204).end()
+// })
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
   if (!body) {
     return res.status(400).json({ error: 'content missing' })
-  } else if (!body.name) {
-    return res.status(400).json({ error: 'name missing' })
-  } else if (!body.number) {
-    return res.status(400).json({ error: 'number missing' })
-  } else if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' })
   }
-  const person = {
+  // else if (!body.name) {
+  //   return res.status(400).json({ error: 'name missing' })
+  // } else if (!body.number) {
+  //   return res.status(400).json({ error: 'number missing' })
+  // } else if (persons.find(person => person.name === body.name)) {
+  //   return res.status(400).json({ error: 'name must be unique' })
+  // }
+  const person = new entry({
     name: body.name,
     number: body.number,
     date: new Date(),
-    id: Math.floor(Math.random() * 10000)
-  }
-  persons = persons.concat(person)
-  res.json(person)
+  })
+  person.save().then(result => {
+    res.json(result.toJSON())
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://bobbyzhou:${password}@cluster0-gwidp.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+})
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+if (process.argv.length === 3) {
+  console.log('phonebook:');
+  Entry.find({}).then(result => {
+    result.forEach(entry => {
+      console.log(entry.name, entry.number, entry._id)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const entry = new Entry({
+    name: name,
+    number: number,
+    date: new Date(),
+  })
+
+  entry.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
